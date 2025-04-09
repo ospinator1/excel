c97a40214767f32d85e583cc19806367270cd306
Stas <loktiost@gmail.com>
2025-04-09 12:23:30 +0300

    расстановка кружков по 5 штук слева и справо и добавление что после хода двигать кружок нельзя

--- excel/MyForm.h
+++ excel/MyForm.h
@@ -12,29 +12,45 @@ public ref class CircleForm : public Form
 private:
     List<Point>^ circleCenters;
     List<bool>^ isDraggingFlags;
+    List<bool>^ wasMovedFlags;  // Флаг, указывающий, был ли кружок перемещен
     int circleRadius;
     int draggedCircleIndex;
 
 public:
     CircleForm()
     {
-        this->Text = "Movable Circles";
+        this->Text = "Movable Circles (One-time move)";
         this->DoubleBuffered = true;
         this->ClientSize = System::Drawing::Size(800, 600);
 
         circleRadius = 30;
         circleCenters = gcnew List<Point>();
         isDraggingFlags = gcnew List<bool>();
+        wasMovedFlags = gcnew List<bool>();
         draggedCircleIndex = -1;
 
-        // Initialize 10 circles in random positions
-        Random^ rand = gcnew Random();
-        for (int i = 0; i < 10; i++)
+        // Calculate vertical spacing between circles
+        int verticalSpacing = this->ClientSize.Height / 6;
+        int horizontalMargin = 100;
+
+        // Initialize 5 circles on the left side
+        for (int i = 1; i <= 5; i++)
         {
-            int x = rand->Next(circleRadius, this->ClientSize.Width - circleRadius);
-            int y = rand->Next(circleRadius, this->ClientSize.Height - circleRadius);
+            int x = horizontalMargin;
+            int y = i * verticalSpacing;
             circleCenters->Add(Point(x, y));
             isDraggingFlags->Add(false);
+            wasMovedFlags->Add(false);
+        }
+
+        // Initialize 5 circles on the right side
+        for (int i = 1; i <= 5; i++)
+        {
+            int x = this->ClientSize.Width - horizontalMargin;
+            int y = i * verticalSpacing;
+            circleCenters->Add(Point(x, y));
+            isDraggingFlags->Add(false);
+            wasMovedFlags->Add(false);
         }
 
         // Set up event handlers
@@ -53,7 +69,9 @@ private:
         // Draw all circles
         for (int i = 0; i < circleCenters->Count; i++)
         {
-            Color circleColor = (i == draggedCircleIndex) ? Color::Red : Color::Blue;
+            // Кружки, которые уже перемещали, будут зелеными
+            Color circleColor = wasMovedFlags[i] ? Color::Green :
+                (i == draggedCircleIndex ? Color::Red : Color::Blue);
             Brush^ brush = gcnew SolidBrush(circleColor);
 
             g->FillEllipse(brush,
@@ -75,21 +93,25 @@ private:
         // Check if mouse is inside any circle
         for (int i = 0; i < circleCenters->Count; i++)
         {
-            int dx = e->X - circleCenters[i].X;
-            int dy = e->Y - circleCenters[i].Y;
-            if (dx * dx + dy * dy <= circleRadius * circleRadius)
+            // Проверяем, что кружок еще не был перемещен
+            if (!wasMovedFlags[i])
             {
-                isDraggingFlags[i] = true;
-                draggedCircleIndex = i;
-                this->Cursor = Cursors::Hand;
-                break;
+                int dx = e->X - circleCenters[i].X;
+                int dy = e->Y - circleCenters[i].Y;
+                if (dx * dx + dy * dy <= circleRadius * circleRadius)
+                {
+                    isDraggingFlags[i] = true;
+                    draggedCircleIndex = i;
+                    this->Cursor = Cursors::Hand;
+                    break;
+                }
             }
         }
     }
 
     void Form_MouseMove(Object^ sender, MouseEventArgs^ e)
     {
-        if (draggedCircleIndex != -1 && isDraggingFlags[draggedCircleIndex])
+        if (draggedCircleIndex != -1 && isDraggingFlags[draggedCircleIndex] && !wasMovedFlags[draggedCircleIndex])
         {
             circleCenters[draggedCircleIndex] = e->Location;
             this->Invalidate(); // Force redraw
@@ -101,8 +123,11 @@ private:
         if (draggedCircleIndex != -1)
         {
             isDraggingFlags[draggedCircleIndex] = false;
+            // Помечаем кружок как перемещенный
+            wasMovedFlags[draggedCircleIndex] = true;
             draggedCircleIndex = -1;
             this->Cursor = Cursors::Default;
+            this->Invalidate(); // Обновляем отображение
         }
     }
 };
